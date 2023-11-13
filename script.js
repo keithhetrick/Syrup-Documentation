9ebcf64ce000bcbae4b0f8ab4c76f1bb6805536d
Keith Hetrick <keithjhetrick@gmail.com>
2023-11-13 17:07:44 -0600

    BFS & DFS buttons added – no functionality yet

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ const options = {
       sortMethod: "directed",
     },
   },
-  // can you add boundaries to the network?
-
   interaction: {
     dragNodes: true, // do not allow dragging nodes
     dragView: true, // do not allow dragging the view
@@ -40,38 +38,37 @@ const options = {
   //   showButton: true,
   // },
   physics: {
-    // enabled: false,
     stabilization: {
-      enabled: false,
+      enabled: true,
       iterations: 1000,
       updateInterval: 100,
     },
     hierarchicalRepulsion: {
-      nodeDistance: 150,
+      nodeDistance: 160,
       centralGravity: 0.0,
       springLength: 100,
       springConstant: 0.01,
       damping: 0.1,
     },
-    // forceAtlas2Based: {
-    //   // gravitationalConstant: -50,
-    //   // centralGravity: 0.01,
-    //   // springConstant: 0.08,
-    //   springLength: 100,
-    //   // damping: 0.4,
-    //   avoidOverlap: 1,
-    // },
+    forceAtlas2Based: {
+      gravitationalConstant: -50,
+      // centralGravity: 0.01,
+      // springConstant: 0.08,
+      springLength: 100,
+      // damping: 0.4,
+      avoidOverlap: 2,
+    },
     solver: "hierarchicalRepulsion",
     barnesHut: {
       gravitationalConstant: -80000,
-      centralGravity: 0.3,
+      centralGravity: 0.5,
       // springLength: 95,
       // springConstant: 0.04,
       // damping: 0.09,
-      avoidOverlap: 1,
+      avoidOverlap: 2,
     },
-    timestep: 0.5,
-    adaptiveTimestep: true,
+    // timestep: 0.5,
+    // adaptiveTimestep: true,
   },
   nodes: {
     shape: "dot",
@@ -123,5 +120,107 @@ function getPath(root, selectedNode) {
     currentNode = nodes.find((node) => node.id === currentNode.parentId);
   }
   path.push(root);
+  return path;
+}
+
+// Depth First Search tree button & execute the search by highlighting the path when button is clicked
+const dfsButton = document.createElement("button");
+dfsButton.innerHTML = "Depth First Search";
+
+dfsButton.addEventListener("click", function () {
+  const selectedNodeId = network.getSelectedNodes()[0];
+  const selectedNode = nodes.find(
+    (node) => node.id === selectedNodeId || node.rootId === selectedNodeId
+  );
+  const rootId = selectedNode.rootId || selectedNode.id;
+  const root = nodes.find((node) => node.id === rootId);
+  const path = dfs(root);
+  const pathIds = path.map((node) => node.id);
+  network.selectNodes(pathIds);
+
+  console.log("\nPATH\n", path);
+  console.log("\nPATH IDS\n", pathIds);
+
+  console.log("\nROOT\n", root);
+  console.log("\nFINAL NODE\n", path[path.length - 1]);
+
+  // clearout after every button click & reset variables to default
+  setTimeout(() => {
+    network.unselectAll();
+  }, 200);
+
+  // reset visited nodes to false
+  nodes.forEach((node) => (node.visited = false));
+});
+
+document.body.appendChild(dfsButton);
+
+function dfs(root) {
+  const path = [];
+  const stack = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    if (node.visited) {
+      continue;
+    }
+    node.visited = true;
+    path.push(node);
+    const children = edges
+      .filter((edge) => edge.from === node.id)
+      .map((edge) => nodes.find((node) => node.id === edge.to));
+    stack.push(...children);
+  }
+  return path;
+}
+
+// Breadth First Search tree button & execute the search by highlighting the path when button is clicked
+const bfsButton = document.createElement("button");
+bfsButton.innerHTML = "Breadth First Search";
+
+bfsButton.addEventListener("click", function () {
+  const selectedNodeId = network.getSelectedNodes()[0];
+  const selectedNode = nodes.find(
+    (node) => node.id === selectedNodeId || node.rootId === selectedNodeId
+  );
+  const rootId = selectedNode.rootId || selectedNode.id;
+  const root = nodes.find((node) => node.id === rootId);
+  const path = bfs(root);
+  const pathIds = path.map((node) => node.id);
+  network.selectNodes(pathIds);
+
+  console.log("\nPATH\n", path);
+  console.log("\nPATH IDS\n", pathIds);
+
+  console.log("\nROOT\n", root);
+  console.log("\nFINAL NODE\n", path[path.length - 1]);
+
+  // clearout after every button click & reset variables to default
+  setTimeout(() => {
+    network.unselectAll();
+  }, 200);
+
+  // reset visited nodes to false
+  nodes.forEach((node) => (node.visited = false));
+});
+
+document.body.appendChild(bfsButton);
+
+function bfs(root) {
+  const path = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (node.visited) {
+      continue;
+    }
+    node.visited = true;
+    path.push(node);
+    const children = edges
+      .filter((edge) => edge.from === node.id)
+      .map((edge) => nodes.find((node) => node.id === edge.to));
+    queue.push(...children);
+  }
   return path;
 }
