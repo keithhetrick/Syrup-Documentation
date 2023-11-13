b151547c3e94c9e357ccf3ae78b298ea776438ec
Keith Hetrick <keithjhetrick@gmail.com>
2023-11-13 11:37:57 -0600

    path visualizers added to diagram

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ import { nodes, edges } from "./data.js";
 // Configuration for the network graph
 var options = {
   layout: {
+    improvedLayout: true,
     hierarchical: {
-      direction: "UD",
-      sortMethod: "directed",
+      enabled: true,
       levelSeparation: 150,
       nodeSpacing: 450,
       treeSpacing: 200,
+      blockShifting: true,
+      edgeMinimization: true,
+      parentCentralization: true,
+      direction: "UD",
+      sortMethod: "directed",
     },
+  },
+  interaction: {
+    dragNodes: true, // do not allow dragging nodes
+    dragView: true, // do not allow dragging the view
+    zoomView: true, // do not allow zooming
+    selectable: true, // do not allow selection
+    hover: true, // do not allow hovering
+    navigationButtons: true, // do not show navigation buttons
+    keyboard: true, // do not allow using the keyboard
+    multiselect: false, // do not allow multiselect
+  },
+  physics: {
+    enabled: true,
+    hierarchicalRepulsion: { nodeDistance: 180 },
+    solver: "hierarchicalRepulsion",
   },
   nodes: {
     shape: "box",
@@ -35,3 +55,25 @@ var network = new vis.Network(
   { nodes: new vis.DataSet(nodes), edges: new vis.DataSet(edges) },
   options
 );
+
+// Add event listener to the network
+network.on("selectNode", function (params) {
+  var selectedNodeId = params.nodes[0];
+  var selectedNode = nodes.find((node) => node.id === selectedNodeId);
+  var rootId = selectedNode.rootId;
+  var root = nodes.find((node) => node.id === rootId);
+  var path = getPath(root, selectedNode);
+  var pathIds = path.map((node) => node.id);
+  network.selectNodes(pathIds);
+});
+
+function getPath(root, selectedNode) {
+  var path = [];
+  var currentNode = selectedNode;
+  while (currentNode.id !== root.id) {
+    path.push(currentNode);
+    currentNode = nodes.find((node) => node.id === currentNode.parentId);
+  }
+  path.push(root);
+  return path;
+}
